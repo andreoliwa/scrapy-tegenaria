f5e9589db72d72f0a204438fe329eae677f57469
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-08-05 02:17:04 +0200

    #3 Use item loaders to sanitize data

--- house_spider/spiders/immobilien_scout_24.py
+++ house_spider/spiders/immobilien_scout_24.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
 from house_spider.items import HouseItem
 
 CITY = ' Berlin,'
@@ -18,19 +19,19 @@ class ImmobilienScout24Spider(scrapy.Spider):
             yield scrapy.Request(link.url, callback=self.parse_item)
 
     def parse_item(self, response):
-        item = HouseItem()
-        item['url'] = response.url
-        item['id'] = int(''.join(response.css('div ul.is24-ex-id li::text').extract()).strip())
-        item['title'] = ''.join(response.css('h1#expose-title::text').extract())
+        item = ItemLoader(HouseItem(), response=response)
+        item.add_value('url', response.url)
+        item.add_xpath('id', "//div/ul[contains(@class, 'is24-ex-id')]/li[1]/text()")
+        item.add_css('title', 'h1#expose-title::text')
 
         full_address = ''.join(response.xpath("//span[@data-qa='is24-expose-address']/text()").extract()).strip()
         parts = full_address.split(CITY)
         if len(parts) == 1:
-            item['address'] = full_address
+            item.add_value('address', full_address)
         else:
-            item['address'] = (parts[0] + CITY).strip(' ,')
-            item['neighborhood'] = ''.join(parts[1:]).strip(' ,')
+            item.add_value('address', (parts[0] + CITY).strip(' ,'))
+            item.add_value('neighborhood', ''.join(parts[1:]).strip(' ,'))
 
-        item['cold_rent'] = ''.join(response.css('div.is24qa-kaltmiete::text').extract()).strip()
-        item['warm_rent'] = ''.join(response.css('dd.is24qa-gesamtmiete::text').extract()).strip()
-        yield item
+        item.add_css('cold_rent', 'div.is24qa-kaltmiete::text')
+        item.add_css('warm_rent', 'dd.is24qa-gesamtmiete::text')
+        yield item.load_item()
