633e795f33406915539d64a60a42f9025e2c6bca
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-08-05 23:15:23 +0200

    #5 Export results to JSON files

--- house_spider/items.py
+++ house_spider/items.py
@@ -51,9 +51,9 @@ class HouseItem(Item):
 
 
 def json_config(current_file, key):
-    var_dir = os.path.abspath(os.path.join(os.path.dirname(current_file), '../../var'))
+    config_dir = os.path.abspath(os.path.join(os.path.dirname(current_file), '../../var/config'))
     basename_no_ext = os.path.splitext(os.path.basename(current_file))[0]
-    full_name = os.path.join(var_dir, '{}.config.json'.format(basename_no_ext))
+    full_name = os.path.join(config_dir, '{}.json'.format(basename_no_ext))
     with open(full_name) as handle:
         json_content = json.load(handle)
     return json_content.get(key)
