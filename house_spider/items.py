a92b627b61275247eb3cabc4bdd285b6c9e0dc53
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2015-08-05 02:17:04 +0200

    #3 Use item loaders to sanitize data

--- house_spider/items.py
+++ house_spider/items.py
@@ -4,15 +4,45 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
+from string import strip
 
 from scrapy import Item, Field
+from scrapy.loader.processors import Join, MapCompose
+
+
+def sanitize_price(value):
+    """Remove the euro symbol and thousands separator.
+
+    :param value: Value to sanitize.
+    :type value: str
+
+    :return: Clean value.
+    """
+    return value.replace(u'\u20ac', '').replace('.', '')
 
 
 class HouseItem(Item):
-    id = Field()
-    url = Field()
-    title = Field()
-    address = Field()
-    neighborhood = Field()
-    cold_rent = Field()
-    warm_rent = Field()
+    url = Field(
+        output_processor=Join()
+    )
+    id = Field(
+        input_processor=MapCompose(strip),
+        output_processor=Join()
+    )
+    title = Field(
+        output_processor=Join()
+    )
+    address = Field(
+        output_processor=Join()
+    )
+    neighborhood = Field(
+        output_processor=Join()
+    )
+    cold_rent = Field(
+        input_processor=MapCompose(sanitize_price, strip),
+        output_processor=Join()
+    )
+    warm_rent = Field(
+        input_processor=MapCompose(sanitize_price, strip),
+        output_processor=Join()
+    )
