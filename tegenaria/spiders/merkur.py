6f6caaa785b44cdf868d1260766fb19db04a9c4b
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-03-24 00:36:02 +0100

    Fix Merkur spider

--- tegenaria/spiders/merkur.py
+++ tegenaria/spiders/merkur.py
@@ -8,31 +8,44 @@ from tegenaria.items import ApartmentItem
 
 
 class MerkurSpider(CrawlSpider):
-    """Apartments from the Merkur Berlin real estate agency."""
+    """Apartments from the Merkur Berlin real estate agency.
+
+    @url http://www.merkur-berlin.de/?page_id=39
+    @returns items 0 0
+    @returns requests 1 10
+    """
 
     name = 'merkur'
     allowed_domains = ['merkur-berlin.de']
-    start_urls = ['http://www.merkur-berlin.de/?page_id=39']
+    start_urls = [
+        # Mietwohnungen on the left menu
+        'http://www.merkur-berlin.de/?page_id=39'
+    ]
 
     rules = (
-        Rule(LinkExtractor(allow=r'ic_estate_view=[0-9]+&estate_id=[0-9]+'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'exposeID=[\dA-F]+'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        """Parse a page with an apartment."""
+        """Parse a page with an apartment.
+
+        @url http://www.merkur-berlin.de/?page_id=39&showExpose=1&exposeID=1F97CD54C9974D7B90C2A354241EEBE4
+        @returns items 1 1
+        @scrapes url title address rooms size warm_rent cold_rent description equipment location other
+        """
         item = ItemLoader(ApartmentItem(), response=response)
         item.add_value('url', response.url)
         item.add_xpath('title', '//h4[@class="entry-title"]/text()')
         item.add_xpath('address', '//address/text()')
 
-        for field, value in dict(rooms='NumberOfRooms', size='LivingSpace',
-                                 warm_rent='TotalRent', cold_rent='BaseRent').items():
-            item.add_css(field, 'tr#ImmocasterContentObjectInfoTable-{}'
-                                ' td.ImmocasterContentObjectInfoTableRgt::text'.format(value))
+        for field, info in dict(rooms='Rooms', size='AreaLiving', warm_rent='PriceWarmmiete',
+                                cold_rent='Price').items():
+            item.add_xpath(field, '//div[@class="infotables"]//tr[@id="infotable_{info}"]/td[@class='
+                                  '"infotable_value"]/text()'.format(info=info))
 
-        for field, value in dict(description='desc', equipment='furnishing',
-                                 location='location', other='other').items():
-            item.add_xpath(field, '//div[@class="merkur_infoblock"]'
-                                  '[@style="immocaster_infoblock_{}"]/p/text()'.format(value))
+        for field, h2 in dict(description='Objekt', equipment='Ausstattung',
+                              location='Lage', other='Mehr Angebote').items():
+            item.add_xpath(field, '//div[@class="infoblock"]/h2[starts-with(normalize-space(.),'
+                                  ' "{h2}")]/following-sibling::p/text()'.format(h2=h2))
 
         return item.load_item()
