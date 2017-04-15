4d8e77d21a09811407d1f29df13513f269a5a423
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-04-16 01:36:14 +0200

    Fix Akelius spider

--- tegenaria/spiders/akelius.py
+++ tegenaria/spiders/akelius.py
@@ -3,6 +3,7 @@
 import re
 
 import requests
+from lxml import etree
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 from scrapy.spiders import CrawlSpider, Rule
@@ -16,48 +17,45 @@ class AkeliusSpider(CrawlSpider, CleanMixin):
 
     name = 'akelius'
     allowed_domains = ['akelius.de']
-    start_urls = ['https://akelius.de/en/search/apartments/osten/berlin/;list/']
+    start_urls = [
+        'https://www.akelius.de/en/search/apartments/osten/berlin/list'
+    ]
 
     rules = (
         Rule(LinkExtractor(allow=r'berlin/[0-9]+/'), callback='parse_item'),
     )
 
-    field_regex = dict(
-        title=re.compile(r'\s*(?P<rooms>[0-9.]+)\s*Room[s], [(Bezirk)]\s*(?P<neighborhood>.*)'),
-        availability=re.compile(r'\s*Available from\s*(?P<availability>.+)[\s\t]*')
-    )
-
-    ADDRESS_REGEX = re.compile(r'markers.+width.+?>(.+)<br \\/>(.+)<.+div>')
+    ADDRESS_REGEX = re.compile(r'<div class="g-map-marker".+<p>.+</div>.+infowindow', re.DOTALL)
 
     def parse_item(self, response):
-        """Parse a page with an apartment."""
+        """Parse a page with an apartment.
+
+        @url https://www.akelius.de/en/search/apartments/osten/berlin/2.7037.16
+        @returns items 1 1
+        @scrapes url title warm_rent size availability cold_rent description address
+        """
         self.shutdown_on_error()
         item = ItemLoader(ApartmentItem(), response=response)
         item.add_value('url', response.url)
-        item.add_css('title', 'article.node.asset.view > header > h2::text')
-        item.add_css('warm_rent', 'div.metadata > p.fact-netrent > span.label-text::text')
-        item.add_css('size', 'div.metadata > p.fact-size::text')
-        item.add_css('location', 'div.metadata > p.fact-location::text')
-        item.add_css('availability', 'div.metadata > p.fact-availability::text')
+        item.add_xpath('title', '//h2/text()')
+        item.add_xpath('warm_rent', '//h2/following-sibling::p[starts-with(normalize-space(.), "Total rent")]/text()')
+        item.add_xpath('size', '//h2//following-sibling::p[2]/text()')
+        item.add_xpath('location',
+                       '//h3[starts-with(normalize-space(.), "Location")]/following-sibling::div//span/text()')
+        item.add_xpath('availability', '//h2//following-sibling::p[4]/text()')
+        item.add_xpath('cold_rent',
+                       '//h3[starts-with(normalize-space(.), "Apartment")]/following-sibling::div[1]/p[2]/span/text()')
+        item.add_xpath('description',
+                       '//h3[starts-with(normalize-space(.), "Building")]/following-sibling::div//span/text()')
 
-        keys = response.css('div.set > p.features > span.bundle > strong.key > span.factlabel::text').extract()
-        values = response.css('div.set > p.features > span.bundle > span.value::text').extract()
-        features = dict(zip(keys, values))
-        item.add_value('cold_rent', features.get('Net Rent'))
+        # The map is shown with JavaScript; get the HTML
+        # and use a regex to extract the part of the script with the address.
+        map_response = requests.get(response.url + '/karte')
+        if map_response.status_code == 200:
+            html_string = ''.join(self.ADDRESS_REGEX.findall(map_response.text))
 
-        item.add_value('description',
-                       u''.join(sorted([u'{}: {}\n'.format(key, value) for key, value in features.items()])))
+            # Extract the address from the HTML.
+            root = etree.fromstring(html_string, etree.HTMLParser())
+            item.add_value('address', ', '.join(root.xpath('//p/text()')))
 
-        map_response = requests.get(response.url + u'map/')
-        match = self.ADDRESS_REGEX.search(map_response.content)
-        item.add_value('address', ', '.join(match.groups()))
-
-        item_dict = item.load_item()
-
-        # After loading: get rooms and neighborhood from the title, clean availability date.
-        for field, regex in self.field_regex.items():
-            match = regex.match(item_dict.get(field))
-            if match:
-                item_dict.update(match.groupdict())
-
-        return item_dict
+        return item.load_item()
