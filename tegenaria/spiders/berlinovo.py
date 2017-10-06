6ff1feb024d90409d408fcbc41fdc684e2607a19
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-10-07 01:27:21 +0200

    Turn Berlinovo into a CrawlSpider (WIP)

--- tegenaria/spiders/berlinovo.py
+++ tegenaria/spiders/berlinovo.py
@@ -1,51 +1,48 @@
 # -*- coding: utf-8 -*-
-"""A spider to crawl the Berlinovo website."""
+"""Furnished and regular apartments from Berlinovo."""
 import re
 
-from scrapy import Request, Spider
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
 
 from tegenaria.items import ApartmentItem
 from tegenaria.spiders import CleanMixin
 
 
-class BerlinovoSpider(Spider, CleanMixin):
-    """A spider to crawl the Berlinovo website."""
+class BerlinovoSpider(CrawlSpider, CleanMixin):
+    """Furnished and regular apartments from Berlinovo."""
 
     name = 'berlinovo'
     allowed_domains = ['berlinovo.de']
-    start_urls = (
+    start_urls = [
+        # Furnished apartments
         'https://www.berlinovo.de/en/suche-apartments',
+        # Regular housing
+        'https://www.berlinovo.de/en/suche-wohnungen',
+    ]
+    rules = (
+        # Furnished apartments
+        Rule(LinkExtractor(allow=r'/en/suche-(apartments|wohnungen).+page=')),
+        Rule(LinkExtractor(allow=r'/en/apartment/'), callback='parse_furnished', follow=True),
+        Rule(LinkExtractor(allow=r'/en/wohnung/'), callback='parse_regular', follow=True),
     )
-    searched_pages = set()
 
-    def parse(self, response):
-        """Parse a search result HTML page.
+    def parse_common(self, response):
+        self.shutdown_on_error()
+        item = ItemLoader(ApartmentItem(), response=response)
+        item.add_value('url', response.url)
+        item.add_css('title', 'h1.title::text')
+        return item
 
-        @url https://www.berlinovo.de/en/suche-apartments
-        @returns items 0 0
-        @returns requests 15 20
-        """
-        for link in LinkExtractor(allow=r'/en/suche-apartments.+page=', unique=True).extract_links(response):
-            if link.url not in (self.searched_pages, self.start_urls):
-                self.searched_pages.add(link.url)
-                yield Request(link.url, callback=self.parse)
-
-        for link in LinkExtractor(allow=r'/en/apartment/', unique=True).extract_links(response):
-            yield Request(link.url, callback=self.parse_item)
-
-    def parse_item(self, response):
+    def parse_furnished(self, response):
         """Parse an ad page, with an apartment.
 
         @url https://www.berlinovo.de/en/apartment/2-room-suite-house-heinrich-heine-stra-e-18-24-berlin-mitte
         @returns items 1 1
         @scrapes url title description location address other neighborhood rooms
         """
-        self.shutdown_on_error()
-        item = ItemLoader(ApartmentItem(), response=response)
-        item.add_value('url', response.url)
-        item.add_css('title', 'h1.title::text')
+        item = self.parse_common(response)
         item.add_xpath('description', '//div[contains(@class, field-name-body)]/div/div[4]/div/div/p/text()')
         item.add_xpath('location', '//div[contains(@class, field-name-field-position)]/div/div[5]/div[2]/div/text()')
 
@@ -70,5 +67,18 @@ class BerlinovoSpider(Spider, CleanMixin):
         room_list = response.xpath('//*[@id="block-views-aktuelle-wohnung-block-3"]/div/div/div/div/div'
                                    '[contains(@class, views-field-field-rooms-description)]/div/text()').extract()
         item.add_value('rooms', re.findall(r'([0-9]+)', ' '.join(room_list))[0])
+
+        yield item.load_item()
+
+    def parse_regular(self, response):
+        """Parse a regular housing apartment ad.
+
+        @url https://www.berlinovo.de/en/wohnung/single-wohnung-hellersdorf-zu-vermieten#history-back
+        @returns items 1 1
+        @scrapes url title description equipment
+        """
+        item = self.parse_common(response)
+        item.add_xpath('description', '//div[contains(@class, field-name-field-description)]/div/div/p/text()')
+        item.add_xpath('equipment', '//div[starts-with(normalize-space(.), "Ausstattung")]//div/text()')
 
         yield item.load_item()
