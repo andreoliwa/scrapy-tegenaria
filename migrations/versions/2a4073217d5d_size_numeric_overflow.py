d410db2a7c8ba21dc4caa602ceb6def3402f9b97
Wagner Augusto Andreoli <andreoliwa@gmail.com>
2017-10-27 01:44:06 +0200

    Fix numeric field overflow (#225)
    * Fix numeric field overflow (closes #225)
    
    * Upgrade requirements

---
+++ migrations/versions/2a4073217d5d_size_numeric_overflow.py
@@ -0,0 +1,25 @@
+"""Size column with numeric overflow (https://github.com/andreoliwa/scrapy-tegenaria/issues/225).
+
+Create Date: 2017-10-27 01:16:21.177093
+"""
+import sqlalchemy as sa
+from alembic import op
+
+revision = '2a4073217d5d'
+down_revision = '87be69f7b2e2'
+
+
+def upgrade():
+    """Apply changes to a database (create tables, columns, etc.)."""
+    op.alter_column('apartment', 'size',
+                    existing_type=sa.NUMERIC(precision=6, scale=2),
+                    type_=sa.Numeric(precision=7, scale=2),
+                    existing_nullable=True)
+
+
+def downgrade():
+    """Reverse actions performed during upgrade."""
+    op.alter_column('apartment', 'size',
+                    existing_type=sa.Numeric(precision=7, scale=2),
+                    type_=sa.NUMERIC(precision=6, scale=2),
+                    existing_nullable=True)
